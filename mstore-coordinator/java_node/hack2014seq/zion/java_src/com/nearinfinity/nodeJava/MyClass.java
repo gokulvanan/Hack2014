f0335ae782b56935ff1667e04cd33ed55a4aa057
gokulvanan <gokulvanan@gmail.com>
2014-09-21 09:23:42 +0530

    removed old coordinator

--- mstore-coordinator/java_node/hack2014seq/zion/java_src/com/nearinfinity/nodeJava/MyClass.java
+++
@@ -1,8 +0,0 @@
-package com.nearinfinity.nodeJava;
-
-public class MyClass {
-  public static String addNumbers(String a) {
-    // return a + b;
-    return a;
-  }
-}
--- mstore-coordinator/java_node/hack2014seq/zion/runMyClass.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-var java = require("../../");
-java.classpath.push("java_src");
-
-var MyClass = java.import("com.nearinfinity.nodeJava.MyClass");
-
-var result = MyClass.addNumbersSync("10");
-console.log(result);
